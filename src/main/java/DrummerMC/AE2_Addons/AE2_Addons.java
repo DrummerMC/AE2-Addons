876eabcf86c2ae37eeac89db90bfeb3f39af7e79
DrummerMC <drummermods@icloud.com>
2014-05-25 15:15:57 +0200

    Upload firs Code

---
+++ src/main/java/DrummerMC/AE2_Addons/AE2_Addons.java
@@ -0,0 +1,18 @@
+package DrummerMC.AE2_Addons;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AE2_Addons.MODID, version = AE2_Addons.VERSION)
+public class AE2_Addons
+{
+    public static final String MODID = "ae2_addons";
+    public static final String VERSION = "@VERSION@";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
